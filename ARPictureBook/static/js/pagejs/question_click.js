7f7d93c351e577e5e305703d9125a91be301d624
CharlesWang1999 <charleswang19991113@gmail.com>
2022-10-02 12:40:55 +0800

    question page style

--- ARPictureBook/static/js/pagejs/question_click.js
+++ ARPictureBook/static/js/pagejs/question_click.js
@@ -1,18 +1,19 @@
-$('#question_button').click(function(){
+$('#question_button').click(function () {
   var resultData = {}
   resultData['uaid'] = $("#uaid").text();
   resultData['round_num'] = $("#round_num").text();
   resultData['page_round'] = $("#page_round").text();
+  console.log('@6--', $("#question_form").serialize())
   $.ajax({
-    url:'/ARPicture/question_click/',
-    type:'POST',
+    url: '/ARPicture/question_click/',
+    type: 'POST',
     data: resultData,
     datatype: 'json',
-    success:function (response) {
+    success: function (response) {
       console.log(response);
-      if(resultData['page_round'] === 'link') {
-        if(response['running_mode'] === 'Testing'){
-          if(response['have_next_page']) {
+      if (resultData['page_round'] === 'link') {
+        if (response['running_mode'] === 'Testing') {
+          if (response['have_next_page']) {
             url = "/ARPicture/question_link/" + resultData['uaid'] + '/' + response['next_round_num'] + '/'
           } else {
             url = "/ARPicture/question_s2/" + resultData['uaid'] + '/1/'
@@ -20,10 +21,10 @@ $('#question_button').click(function(){
         } else {
           url = "/ARPicture/smooth_music/" + resultData['uaid'] + '/' + resultData['round_num'] + '/link/'
         }
-      } else{
+      } else {
         url = "/ARPicture/self_report_" + resultData['page_round'] + "/" + resultData['uaid'] + '/' + resultData['round_num'] + '/'
       }
-      window.location.href = url;
+      // window.location.href = url;
     }
   })
 }
