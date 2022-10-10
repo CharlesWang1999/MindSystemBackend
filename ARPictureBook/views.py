e8d9feea53ead1c86e8bc1b668646ba359ad4f05
Bubbleinpit <bubbleinpit@gmail.com>
2022-10-10 16:29:35 +0800

    feat: video caption

--- ARPictureBook/views.py
+++ ARPictureBook/views.py
@@ -392,7 +392,12 @@ def question_s2_view(request, uaid, round_num,running_mode):
         str_write=str(start_time)+' question_s2阶段'+'第'+str(round_num)+'页'+" 图片出现\n"
         f.write(str_write)
     start_record(uaid, 's2', round_num)
-    return render(request, 'question_s2.html', {'uaid': uaid, 'round_num': round_num, 'video_name': 'sub01-1.mp4'})
+    return render(request, 'question_s2.html', {
+        'uaid': uaid,
+        'round_num': round_num,
+        'video_name': 'sub01-1.mp4',
+        'running_mode': running_mode,
+    })
 
 
 @login_required
